f2a3ea47769ebf2bd25f2dd3c1f987eff7fe6dfa
nadiol <samanafatta@yahoo.com>
2025-04-19 11:22:09 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-<?php 
+<?php include 'includes/header.php'; ?>
 include_once 'utils.php';
-include 'includes/header.php'; 
-?>
 <h1>Benvenuto nell'Archivio D&D (PHP)</h1>
 <p><a href="gestione.php">Vai alla gestione contenuti</a></p>
 <?php include 'includes/footer.php'; ?>
