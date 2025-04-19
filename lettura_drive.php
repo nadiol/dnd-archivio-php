42d905df4616579216d990da803de7b3716bcb3d
nadiol <samanafatta@yahoo.com>
2025-04-19 16:44:18 +0200

    Add files via upload

--- lettura_drive.php
+++ lettura_drive.php
@@ -2,22 +2,36 @@
 include_once 'utils.php';
 include 'includes/header.php'; 
 
-$index_url = "https://drive.google.com/uc?export=download&id=14QzWfVXu-MPlq0YxTCXJ-NV356hdXU1T"; // drive_index.json
-$index_data = json_decode(file_get_contents($index_url), true);
-$scelto = $_GET['file'] ?? '';
-$dati = [];
+$index_url = "https://drive.google.com/uc?export=download&id=14QzWfVXu-MPlq0YxTCXJ-NV356hdXU1T";
+$json_raw = @file_get_contents($index_url);
 
-$map_file_id = [];
-foreach ($index_data as $entry) {
-    $map_file_id[$entry['nome']] = $entry['id'];
+$index_data = null;
+if (!$json_raw) {
+    echo "<p style='color:red;'>⚠️ Errore: impossibile scaricare il file index da Google Drive.</p>";
+} else {
+    $index_data = json_decode($json_raw, true);
+    if (!$index_data) {
+        echo "<p style='color:red;'>⚠️ Errore: il file scaricato non è un JSON valido.</p>";
+        echo "<pre>" . htmlspecialchars($json_raw) . "</pre>";
+    }
 }
 
-if ($scelto && isset($map_file_id[$scelto])) {
-    $id = $map_file_id[$scelto];
-    $url = "https://drive.google.com/uc?export=download&id=$id";
-    $json = file_get_contents($url);
-    if ($json) {
-        $dati = json_decode($json, true);
+$scelto = $_GET['file'] ?? '';
+$dati = [];
+$map_file_id = [];
+
+if ($index_data) {
+    foreach ($index_data as $entry) {
+        $map_file_id[$entry['nome']] = $entry['id'];
+    }
+
+    if ($scelto && isset($map_file_id[$scelto])) {
+        $id = $map_file_id[$scelto];
+        $url = "https://drive.google.com/uc?export=download&id=$id";
+        $json = file_get_contents($url);
+        if ($json) {
+            $dati = json_decode($json, true);
+        }
     }
 }
 ?>
