0236618f3bb2b0cadb4e32055c2dfe1cc6b8b3e2
nadiol <samanafatta@yahoo.com>
2025-04-19 11:33:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<?php include 'includes/header.php'; ?>
+<?php 
 include_once 'utils.php';
+include 'includes/header.php'; 
+?>
 <h1>Benvenuto nell'Archivio D&D (PHP)</h1>
 <p><a href="gestione.php">Vai alla gestione contenuti</a></p>
 <?php include 'includes/footer.php'; ?>
