aca2da0eb89a9ef02c9a0324fa455ef8a4f1b523
nadiol <samanafatta@yahoo.com>
2025-04-19 18:01:30 +0200

    Add files via upload

--- lettura_drive.php
+++ lettura_drive.php
@@ -13,7 +13,7 @@ function scarica_da_drive($id) {
     return $output;
 }
 
-$json_raw = scarica_da_drive("_FqDS1q3XmOHeJf46TtqGWgGq69IQik2");
+$json_raw = scarica_da_drive("1_FqDS1q3XmOHeJf46TtqGWgGq69IQik2");
 
 $index_data = null;
 if (!$json_raw) {
