a38b996bca444ddce699e5d26655a54fd2803d7c
nadiol <samanafatta@yahoo.com>
2025-04-19 16:46:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 include_once 'utils.php';
 include 'includes/header.php'; 
 ?>
-<h1>Benvenuto nell'Archivio D&D (PHP)</h1>
-<ul>
-  <li><a href="gestione.php">Gestione contenuti</a></li>
-  <li><a href="lettura_drive.php">Visualizza dati da Google Drive</a></li>
+<h1 style="font-family: sans-serif;">Benvenuto nell'Archivio D&D (PHP)</h1>
+<ul style="font-family: sans-serif; font-size: 1.1rem;">
+  <li><a href="gestione.php">🛠️ Gestione contenuti</a></li>
+  <li><a href="lettura_drive.php">📂 Visualizza dati da Google Drive</a></li>
 </ul>
 <?php include 'includes/footer.php'; ?>
